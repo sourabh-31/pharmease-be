93d0cbb2945256a309d9a20bb9f514f82a54aed3
Sourabh <sourabhhaldarh@gmail.com>
2025-12-14 16:26:12 +0530

    fix: changed cookie settings for cross site

--- utils/cookie.js
+++ utils/cookie.js
@@ -1,13 +1,17 @@
 const jwt = require("jsonwebtoken");
 
 const sendCookie = (user, res, message, statusCode = 200) => {
-  const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
+  const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+    expiresIn: "7d", // optional: token expires in 7 days
+  });
 
   res
     .status(statusCode)
     .cookie("token", token, {
       httpOnly: true,
-      maxAge: null,
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in ms
+      secure: process.env.NODE_ENV === "production", // true on HTTPS
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", // None for cross-site in prod
     })
     .json({
       success: true,
