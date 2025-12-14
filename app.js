19318f74f6fd1b92a17b51b140268f8360f2a734
Sourabh <sourabhhaldarh@gmail.com>
2025-12-14 16:53:47 +0530

    feat: updated cors origin to deployed frontend origin

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://pharmease-bysourabh.netlify.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
